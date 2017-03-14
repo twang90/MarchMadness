6bfc52cfb4b6d144e0b7a2202bd8bca8031e667e
YifangZhao <YifangZhao@MacBook-Air.attlocal.net>
2017-03-13 21:59:12 -0400

    New model for team

---
+++ src/test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from team import Team
+
+t1 = Team("Duke",43,15,36.3,47.7,37.6)
+t2 = Team("UNC",46,5,43.5,47.1,36.6)
+
+i = 0
+while i < 10:
+	if i%2 == 0:
+		t1.offense(t2)
+	else:
+		t2.offense(t1)
+	i = i+1
+
+	
