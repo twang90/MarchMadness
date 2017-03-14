ad77876fb22f2a1ad59cc2b25b619ca43c210470
Tiancong Wang <Bernard@MacBook-Pro.attlocal.net>
2017-03-13 22:12:02 -0400

    combine team and game

--- src/sim.py
+++ src/sim.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 
 from game import Game
+from team import Team
 
-rivalry = Game("Duke", "UNC")
+duke = Team("Duke", 43,15,36.3,47.7,37.6, False)
+unc = Team("UNC", 46,5,43.5,47.1,36.6, False)
+rivalry = Game(duke, unc)
 
 rivalry.simulate()
